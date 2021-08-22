138246a58703675543a73b07c4bb28b818274b48
AI913 <dywchiu913@hotmail.com>
2021-08-23 08:09:01 +0900

    Completed except some shadowed titles

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of Dennis is to maximize his potential to fight against China.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
